4ec450a1da7c84f93680144b078da13b11e316f6
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-05-11 19:05:54 -0600

    Adjusted OpenAI keyword prompt, added getCoverLetter controller

--- controllers/getCoverLetter.js
+++ controllers/getCoverLetter.js
@@ -1,16 +1,16 @@
 const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config()
 
-async function getKeyWords (text) {
+async function getCoverLetter (text) {
     const configuration = new Configuration({
         apiKey: process.env.OPEN_AI_KEY,
       })
       const openai = new OpenAIApi(configuration);
       const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `Format your answer in a comma delimited string such as 'javascript, python, node.js'. Do not return anything else. Extract keywords from this text:\n${text}`,
+        prompt: `${text}`,
         temperature: 0.5,
-        max_tokens: 1000,
+        max_tokens: 4000,
         top_p: 1.0,
         frequency_penalty: 0.8,
         presence_penalty: 0.0,
@@ -18,5 +18,3 @@ async function getKeyWords (text) {
       console.log(response.data)
       return response.data
 }
-
-module.exports = getKeyWords
